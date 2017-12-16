d2e955db3549ea1ad6bef828a33a79368f9ae700
Jordan Deam <jordandeam@Jordans-MacBook-Pro.local>
2017-12-15 17:51:13 -0800

    Added styling

--- campare.js
+++ campare.js
@@ -107,31 +107,49 @@ function renderTable(cams) {
 	let strength = document.querySelector('#cam-strength');
 	let price = document.querySelector('#cam-price');
 
-	cams.forEach(function(cam) {
+	cams.forEach(function(cam, i) {
 		let camName = document.createElement('th');
 		camName.textContent = `${cam.brand} ${cam.model} #${cam.id}`;
+		if (i===0) {
+			camName.style.borderRight = '1px solid lightgray';
+		}
 		id.appendChild(camName);
 
 		let camImg = document.createElement('td');
 		let camPic = document.createElement('img');
 		camPic.src = cam.imgURL;
+		if (i===0) {
+			camImg.style.borderRight = '1px solid lightgray';
+		}
 		camImg.appendChild(camPic);
 		img.appendChild(camImg);
 
 		let camRange = document.createElement('td');
 		camRange.textContent = `${cam.rangeMin} mm - ${cam.rangeMax} mm`;
+		if (i===0) {
+			camRange.style.borderRight = '1px solid lightgray';
+		}
 		range.appendChild(camRange);
 
 		let camWeight = document.createElement('td');
 		camWeight.textContent = `${cam.weight} grams`;
+		if (i===0) {
+			camWeight.style.borderRight = '1px solid lightgray';
+		}
 		weight.appendChild(camWeight);
 
 		let camStrength = document.createElement('td');
 		camStrength.textContent = `${cam.strength} kN`;
+		if (i===0) {
+			camStrength.style.borderRight = '1px solid lightgray';
+		}
 		strength.appendChild(camStrength);
 
 		let camPrice = document.createElement('td');
 		camPrice.textContent = `$${cam.price}`;
+		if (i===0) {
+			camPrice.style.borderRight = '1px solid lightgray';
+		}
 		price.appendChild(camPrice);
 	});
 }
@@ -192,15 +210,104 @@ submitButton.addEventListener('click', function(event) {
 	}
 });
 
+function findWidestRange(cams) {
+	let widest = [0];
+	for (let i=1; i<cams.length; i++) {
+		if (cams[i].rangeTotal>cams[widest[0]].rangeTotal) {
+			widest = [i];
+		} else if (cams[i].rangeTotal===cams[widest[0]].rangeTotal) {
+			widest.push(i);
+		}
+	}
+	return widest;
+}
+
+function findLightest(cams) {
+	let lightest = [0]
+	for (let i=1; i<cams.length; i++) {
+		if (cams[i].weight<cams[lightest[0]].weight) {
+			lightest = [i];
+		} else if (cams[i].weight===cams[lightest[0]].weight) {
+			lightest.push(i);
+		}
+	}
+	return lightest;
+}
+
+function findStrongest(cams) {
+	let strongest = [0]
+	for (let i=1; i<cams.length; i++) {
+		if (cams[i].strength>cams[strongest[0]].strength) {
+			strongest = [i];
+		} else if (cams[i].strength===cams[strongest[0]].strength) {
+			strongest.push(i);
+		}
+	}
+	return strongest;
+}
+
+function findCheapest(cams) {
+	let cheapest = [0]
+	for (let i=1; i<cams.length; i++) {
+		if (cams[i].price<cams[cheapest[0]].price) {
+			cheapest = [i];
+		} else if (cams[i].price===cams[cheapest[0]].price) {
+			cheapest.push(i);
+		}
+	}
+	return cheapest;
+}
+
 table.addEventListener('mouseenter', function(event) {
 	let ids = ['cam-range', 'cam-weight', 'cam-strength', 'cam-price']
 	if (ids.includes(event.target.id)) {
+		if (event.target.id==='cam-range') {
+			let widest = findWidestRange(currentResults).map(i => i+1);
+			event.target.childNodes.forEach(function(node, i) {
+				if (widest.includes(i)) {
+					node.style.textDecoration = 'underline';
+					node.style.fontWeight = 'bold'
+				}
+			});
+		}
+		if (event.target.id==='cam-weight') {
+			let lightest = findLightest(currentResults).map(i => i+1);
+			event.target.childNodes.forEach(function(node, i) {
+				if (lightest.includes(i)) {
+					node.style.textDecoration = 'underline';
+					node.style.fontWeight = 'bold';
+				}
+			});
+		}
+		if (event.target.id==='cam-strength') {
+			let strongest = findStrongest(currentResults).map(i => i+1);
+			event.target.childNodes.forEach(function(node, i) {
+				if (strongest.includes(i)) {
+					node.style.textDecoration = 'underline';
+					node.style.fontWeight = 'bold';
+				}
+			});
+		}
+		if (event.target.id==='cam-price') {
+			let cheapest = findCheapest(currentResults).map(i => i+1);
+			event.target.childNodes.forEach(function(node, i) {
+				if (cheapest.includes(i)) {
+					node.style.textDecoration = 'underline';
+					node.style.fontWeight = 'bold';
+				}
+			});
+		}
+
 		event.target.classList.add('is-selected');
 	}
 }, true);
 
 table.addEventListener('mouseleave', function(event) {
 	if (event.target.classList.contains('is-selected')) {
+		event.target.childNodes.forEach(function(node) {
+			node.style.textDecoration = '';
+			node.style.fontWeight = '';
+		});
 		event.target.classList.remove('is-selected');
 	}
 }, true);
